8d15a8c3c146b45f3b9207101070b37683e23597
Nicholas Warfield <warfield9000@gmail.com>
2019-09-24 22:57:12 -0700

    Filter Bar Created an app bar, I want it to hide on scroll but that isn't working.

---
+++ src/components/FilterBar.vue
@@ -0,0 +1,24 @@
+<template>
+	<v-card class="overflow-hidden">
+		<v-app-bar
+			absolute
+			color="red darken-3"
+			dark
+			hide-on-scroll
+			scroll-target="results"
+		>
+			<v-app-bar-nav-icon></v-app-bar-nav-icon>
+			<v-toolbar-title>Filter Bar</v-toolbar-title>
+			<div class="flex-grow-1"></div>
+
+		</v-app-bar>
+
+		<v-sheet
+			id="results"
+			class="overflow-y-auto"
+			max-height="600"
+		>
+			<v-container style="height: 1000px;"></v-container>
+		</v-sheet>
+	</v-card>
+</template>
