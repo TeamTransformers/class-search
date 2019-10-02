a2e32d81f692c2db4c9bff2e6bf8c76747a5175c
co8mcate <colin.mcateer.26@my.csun.edu>
2019-10-02 10:49:00 -0700

    separate codes from two components

--- src/components/Course.vue
+++ src/components/Course.vue
@@ -5,48 +5,9 @@
         <v-expansion-panel-content>
           <v-card class="d-flex flex-row justify-start align-center">
         <v-card-title>
-          <v-simple-table 
-          :dense="dense" 
-          :fixed-header="fixedHeader"
-          :height="height">
-            <template v-slot:default>
-              <thead>
-                <tr>
-                  <th class="text-left">Detail</th>
-                  <th class="text-left">Session</th>
-                  <th class="text-left">Section</th>
-                  <th class="text-left">Textbooks</th>
-                  <th class="text-left">Class #</th>
-                  <th class="text-left">Seats available</th>
-                  <th class="text-left">Enrolled status</th>
-                  <th class="text-left">Comp</th>
-                  <th class="text-left">Location</th>
-                  <th class="text-left">Days/Time</th>
-                  <th class="text-left">Instructor</th>
-                  <th class="text-left">Consent</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr v-for="item in courses" :key="item.session">
-                  <td>{{ item.detail }}</td>
-                  <td class="text-center">{{ item.session }}</td>
-                  <td class="text-center">{{ item.section }}</td>
-                  <td>{{ item.textbook }}</td>
-                  <td class="text-center">{{ item.classnum }}</td>
-                  <td class="text-center">{{ item.seats }}</td>
-                  <td class="text-center">{{ item.status }}</td>
-                  <td class="text-center">{{ item.comp }}</td>
-                  <td class="text-center">{{ item.loc }}</td>
-                  <td>{{ item.day }}{{ item.time }}</td>
-                  <td>{{ item.instructor }}</td>
-                  <td>{{ item.content }}</td>
-                </tr>
-              </tbody>
-            </template>
-          </v-simple-table>
+          <Section></Section>
         </v-card-title>
-        <!-- Insert Section Information Component here -->
-        <b-table striped hover :items="items"></b-table>
+        <!-- Insert Section Information Component here -->  
       </v-card>
         </v-expansion-panel-content>
   </v-expansion-panel>
@@ -54,83 +15,13 @@
 </template>
 
 <script>
+
+import Section from './Section'
+
   export default {
+    components: { Section },
     data: () => ({
-      dense: false,
-      fixedHeader: false,
-      height: 300,
-      courses: [
-        {
-          detail: null,
-          session: 1,
-          section: 1,
-          textbook: null,
-          classnum: 16320,
-          seats: '-10',
-          status: 'closed',
-          comp: 'LEC',
-          loc: 'JD3510',
-          day: 'Tu/Th',
-          instructor: 'McInhenny, Robert D',
-          content: null,
-        },
-        {
-          detail: null,
-          session: 1,
-          section: 2,
-          textbook: null,
-          classnum: 16321,
-          seats: '-2',
-          status: 'closed',
-          comp: 'LEC',
-          loc: 'JD1618',
-          day: 'Tu/Th',
-          instructor: 'Lazik, George L',
-          content: null,
-        },
-        {
-          detail: null,
-          session: 1,
-          section: 3,
-          textbook: null,
-          classnum: 16839,
-          seats: '-7',
-          status: 'closed',
-          comp: 'LEC',
-          loc: 'JD1618',
-          day: 'Tu/Th',
-          instructor: 'Lazik, George L',
-          content: null,
-        },
-        {
-          detail: null,
-          session: 1,
-          section: 4,
-          textbook: null,
-          classnum: 17067,
-          seats: '0',
-          status: 'closed',
-          comp: 'LEC',
-          loc: 'JD1600',
-          day: 'Tu/Th',
-          instructor: 'Lazik, George L',
-          content: null,
-        },
-        {
-          detail: null,
-          session: 1,
-          section: 5,
-          textbook: null,
-          classnum: 21293,
-          seats: '-2',
-          status: 'closed',
-          comp: 'LEC',
-          loc: 'JD3510',
-          day: 'Tu',
-          instructor: 'Bhatt, Vijay J',
-          content: null,
-        },
-      ],
+      
     }),
   }
 </script>
--- src/components/Section.vue
+++ src/components/Section.vue
@@ -1,22 +1,124 @@
 <template>
-  <div>
-    <b-table striped hover :items="items" :fields="fields"></b-table>
-  </div>
+  <v-simple-table 
+    dense="dense" 
+    :fixed-header="fixedHeader"
+    :height="height">
+    <template v-slot:default>
+      <thead>
+        <tr>
+          <th class="text-left">Detail</th>
+          <th class="text-left">Session</th>
+          <th class="text-left">Section</th>
+          <th class="text-left">Textbooks</th>
+          <th class="text-left">Class #</th>
+          <th class="text-left">Seats available</th>
+          <th class="text-left">Enrolled status</th>
+          <th class="text-left">Comp</th>
+          <th class="text-left">Location</th>
+          <th class="text-left">Days/Time</th>
+          <th class="text-left">Instructor</th>
+          <th class="text-left">Consent</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="item in courses" :key="item.session">
+          <td>{{ item.detail }}</td>
+          <td class="text-center">{{ item.session }}</td>
+          <td class="text-center">{{ item.section }}</td>
+          <td>{{ item.textbook }}</td>
+          <td class="text-center">{{ item.classnum }}</td>
+          <td class="text-center">{{ item.seats }}</td>
+          <td class="text-center">{{ item.status }}</td>
+          <td class="text-center">{{ item.comp }}</td>
+          <td class="text-center">{{ item.loc }}</td>
+          <td>{{ item.day }}{{ item.time }}</td>
+          <td>{{ item.instructor }}</td>
+          <td>{{ item.content }}</td>
+          </tr>
+      </tbody>
+    </template>  
+  </v-simple-table>
 </template>
+          
 
 <script>
   export default {
-    data() {
-      return {
-        // Note `isActive` is left out and will not appear in the rendered table
-        fields: ['first_name', 'last_name', 'age'],
-        items: [
-          { isActive: true, age: 40, first_name: 'Dickerson', last_name: 'Macdonald' },
-          { isActive: false, age: 21, first_name: 'Larsen', last_name: 'Shaw' },
-          { isActive: false, age: 89, first_name: 'Geneva', last_name: 'Wilson' },
-          { isActive: true, age: 38, first_name: 'Jami', last_name: 'Carney' }
-        ]
-      }
-    }
+    data: () => ({
+      dense: false,
+      fixedHeader: false,
+      height: 300,
+      courses: [
+        {
+          detail: null,
+          session: 1,
+          section: 1,
+          textbook: null,
+          classnum: 16320,
+          seats: '-10',
+          status: 'closed',
+          comp: 'LEC',
+          loc: 'JD3510',
+          day: 'Tu/Th',
+          instructor: 'McInhenny, Robert D',
+          content: null,
+        },
+        {
+          detail: null,
+          session: 1,
+          section: 2,
+          textbook: null,
+          classnum: 16321,
+          seats: '-2',
+          status: 'closed',
+          comp: 'LEC',
+          loc: 'JD1618',
+          day: 'Tu/Th',
+          instructor: 'Lazik, George L',
+          content: null,
+        },
+        {
+          detail: null,
+          session: 1,
+          section: 3,
+          textbook: null,
+          classnum: 16839,
+          seats: '-7',
+          status: 'closed',
+          comp: 'LEC',
+          loc: 'JD1618',
+          day: 'Tu/Th',
+          instructor: 'Lazik, George L',
+          content: null,
+        },
+        {
+          detail: null,
+          session: 1,
+          section: 4,
+          textbook: null,
+          classnum: 17067,
+          seats: '0',
+          status: 'closed',
+          comp: 'LEC',
+          loc: 'JD1600',
+          day: 'Tu/Th',
+          instructor: 'Lazik, George L',
+          content: null,
+        },
+        {
+          detail: null,
+          session: 1,
+          section: 5,
+          textbook: null,
+          classnum: 21293,
+          seats: '-2',
+          status: 'closed',
+          comp: 'LEC',
+          loc: 'JD3510',
+          day: 'Tu',
+          instructor: 'Bhatt, Vijay J',
+          content: null,
+        },
+      ],
+    }),
   }
 </script>
