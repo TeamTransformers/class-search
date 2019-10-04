f6197d0f1687c544cde3a731ba4bd9de2133fb68
shadowgun625 <shadowgun625@gmail.com>
2019-10-03 18:38:17 -0700

    add test.vue to display new urls

---
+++ src/components/Test.vue
@@ -0,0 +1,24 @@
+<template>
+<div>
+    <div 
+    v-for="data in abbr"
+    :key = data
+    >
+    {{"https://api.metalab.csun.edu/curriculum/api/2.0/courses/"+data}}
+    </div>    
+</div>
+</template>
+
+<script>
+import abbrv from "../python/abbrv.json";
+export default {
+  data() {
+    return {
+        abbr: abbrv
+    }
+  },
+};
+</script>
+
+<style>
+</style>
